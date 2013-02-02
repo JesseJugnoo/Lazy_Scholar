edcc662c581e1680b38432147d33d63cc7f7d0fe
william-park <william.park519@gmail.com>
2013-02-02 12:16:11 -0500

    HelloWorld

---
+++ HelloCascades/translations/hellocascades_fr.ts
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>hellocascades</name>
+    <message>
+        <source>Hello World</source>
+        <translation type="obsolete">Bonjour monde</translation>
+    </message>
+    <message>
+        <location filename="../assets/hellocascades.qml" line="50"/>
+        <source>HYDRO PUMP</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
